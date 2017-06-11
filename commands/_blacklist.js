86e5553fada0bb4e7787ac5761a88bd01220df82
Tobias Meister <meister.tobias21@gmail.com>
2017-06-11 19:04:53 +0200

    Replaced cmd _block with _blacklist for proper phrase blacklisting + per guild configuration

---
+++ commands/_blacklist.js
@@ -0,0 +1,63 @@
+const Logger = require('../util/Logger.js')('CMD', '_blacklist');
+const GuildConf = require('../util/GuildConf.js');
+
+let blacklist;
+let markedDirty = true;
+
+exports.run = {};
+
+exports.run.blacklist = (Bot, msg, args) => {
+	if (args.length === 0) {
+		return msg.channel.sendMessage('Need to specify a phrase to blacklist.');
+	}
+
+	let block = args.join(' ');
+
+	let param = {};
+	param[block] = null;
+	GuildConf.set('blacklist', msg.channel.guild.id, param);
+	markedDirty = true;
+};
+
+exports.run.allow = (Bot, msg, args) => {
+	if (args.length === 0) {
+		return msg.channel.sendMessage('Need to specify a phrase to remove from blacklist.');
+	}
+
+	let block = args.join(' ');
+
+	GuildConf.delete('blacklist', msg.channel.guild.id, [ block ]);
+	markedDirty = true;
+};
+
+exports.run.blacklisted = async (Bot, msg, args) => {
+	if (markedDirty) {
+		blacklist = await GuildConf.get('blacklist', msg.channel.guild.id);
+		markedDirty = false;
+	}
+
+	if (Object.keys(blacklist).length === 0) {
+		return msg.channel.send('No blacklisted words.');
+	}
+
+	let arr = Object.keys(blacklist);
+	let str = arr.map(msg => (arr.indexOf(msg) + 1) + '. ' + msg).join('\n');
+
+	msg.channel.send('```Markdown\n'
+			+ 'Blacklisted words:\n'
+			+ '==================\n\n'
+			+ str + '\n```');
+};
+
+exports.run._block = async (Bot, msg) => {
+	if (markedDirty) {
+		blacklist = await GuildConf.get('blacklist', msg.channel.guild.id);
+		markedDirty = false;
+	}
+
+	for (let block in blacklist) {
+		if (msg.content.includes(block)) {
+			return msg.delete();
+		}
+	}
+};
